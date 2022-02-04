d9b7e0c4ac337abbed0fe5733f81fa37ca225784
chu.lu <luchu1993@163.com>
2022-02-05 00:43:02 +0800

    matrix3x4

--- Source/Engine/Math/Matrix3x4.h
+++ Source/Engine/Math/Matrix3x4.h
@@ -6,6 +6,7 @@
 
 #include "Container/String.h"
 #include "Math/Matrix3.h"
+#include "Math/Matrix4.h"
 #include "Math/Vector4.h"
 
 
@@ -85,7 +86,40 @@ namespace My3D
         }
         /// Assign from another matrix.
         Matrix3x4& operator =(const Matrix3x4& rhs) noexcept = default;
-
+        /// Assign from a 3x3 matrix and set the extra elements to identity.
+        Matrix3x4& operator =(const Matrix3& rhs) noexcept
+        {
+            m00_ = rhs.m00_;
+            m01_ = rhs.m01_;
+            m02_ = rhs.m02_;
+            m03_ = 0.0f;
+            m10_ = rhs.m10_;
+            m11_ = rhs.m11_;
+            m12_ = rhs.m12_;
+            m13_ = 0.0f;
+            m20_ = rhs.m20_;
+            m21_ = rhs.m21_;
+            m22_ = rhs.m22_;
+            m23_ = 0.0f;
+            return *this;
+        }
+        /// Assign from a 4x4 matrix which is assumed to contain no projection.
+        Matrix3x4& operator =(const Matrix4& rhs) noexcept
+        {
+            m00_ = rhs.m00_;
+            m01_ = rhs.m01_;
+            m02_ = rhs.m02_;
+            m03_ = rhs.m03_;
+            m10_ = rhs.m10_;
+            m11_ = rhs.m11_;
+            m12_ = rhs.m12_;
+            m13_ = rhs.m13_;
+            m20_ = rhs.m20_;
+            m21_ = rhs.m21_;
+            m22_ = rhs.m22_;
+            m23_ = rhs.m23_;
+            return *this;
+        }
         /// Test for equality with another matrix without epsilon.
         bool operator ==(const Matrix3x4& rhs) const
         {
@@ -102,6 +136,215 @@ namespace My3D
         }
         /// Test for inequality with another matrix without epsilon.
         bool operator !=(const Matrix3x4& rhs) const { return !(*this == rhs); }
+        /// Multiply a Vector3 which is assumed to represent position.
+        Vector3 operator *(const Vector3& rhs) const
+        {
+            return Vector3(
+                (m00_ * rhs.x_ + m01_ * rhs.y_ + m02_ * rhs.z_ + m03_),
+                (m10_ * rhs.x_ + m11_ * rhs.y_ + m12_ * rhs.z_ + m13_),
+                (m20_ * rhs.x_ + m21_ * rhs.y_ + m22_ * rhs.z_ + m23_)
+            );
+        }
+        /// Multiply a Vector4.
+        Vector3 operator *(const Vector4& rhs) const
+        {
+            return Vector3(
+                (m00_ * rhs.x_ + m01_ * rhs.y_ + m02_ * rhs.z_ + m03_ * rhs.w_),
+                (m10_ * rhs.x_ + m11_ * rhs.y_ + m12_ * rhs.z_ + m13_ * rhs.w_),
+                (m20_ * rhs.x_ + m21_ * rhs.y_ + m22_ * rhs.z_ + m23_ * rhs.w_)
+            );
+        }
+        /// Add a matrix.
+        Matrix3x4 operator +(const Matrix3x4& rhs) const
+        {
+            return Matrix3x4(
+                m00_ + rhs.m00_,
+                m01_ + rhs.m01_,
+                m02_ + rhs.m02_,
+                m03_ + rhs.m03_,
+                m10_ + rhs.m10_,
+                m11_ + rhs.m11_,
+                m12_ + rhs.m12_,
+                m13_ + rhs.m13_,
+                m20_ + rhs.m20_,
+                m21_ + rhs.m21_,
+                m22_ + rhs.m22_,
+                m23_ + rhs.m23_
+            );
+        }
+        /// Subtract a matrix.
+        Matrix3x4 operator -(const Matrix3x4& rhs) const
+        {
+            return Matrix3x4(
+                m00_ - rhs.m00_,
+                m01_ - rhs.m01_,
+                m02_ - rhs.m02_,
+                m03_ - rhs.m03_,
+                m10_ - rhs.m10_,
+                m11_ - rhs.m11_,
+                m12_ - rhs.m12_,
+                m13_ - rhs.m13_,
+                m20_ - rhs.m20_,
+                m21_ - rhs.m21_,
+                m22_ - rhs.m22_,
+                m23_ - rhs.m23_
+            );
+        }
+        /// Multiply with a scalar.
+        Matrix3x4 operator *(float rhs) const
+        {
+            return Matrix3x4(
+                m00_ * rhs,m01_ * rhs,m02_ * rhs,m03_ * rhs,
+                m10_ * rhs,m11_ * rhs,m12_ * rhs,m13_ * rhs,
+                m20_ * rhs,m21_ * rhs,m22_ * rhs,m23_ * rhs
+            );
+        }
+        /// Multiply a matrix.
+        Matrix3x4 operator *(const Matrix3x4& rhs) const
+        {
+            return Matrix3x4(
+                m00_ * rhs.m00_ + m01_ * rhs.m10_ + m02_ * rhs.m20_,
+                m00_ * rhs.m01_ + m01_ * rhs.m11_ + m02_ * rhs.m21_,
+                m00_ * rhs.m02_ + m01_ * rhs.m12_ + m02_ * rhs.m22_,
+                m00_ * rhs.m03_ + m01_ * rhs.m13_ + m02_ * rhs.m23_ + m03_,
+                m10_ * rhs.m00_ + m11_ * rhs.m10_ + m12_ * rhs.m20_,
+                m10_ * rhs.m01_ + m11_ * rhs.m11_ + m12_ * rhs.m21_,
+                m10_ * rhs.m02_ + m11_ * rhs.m12_ + m12_ * rhs.m22_,
+                m10_ * rhs.m03_ + m11_ * rhs.m13_ + m12_ * rhs.m23_ + m13_,
+                m20_ * rhs.m00_ + m21_ * rhs.m10_ + m22_ * rhs.m20_,
+                m20_ * rhs.m01_ + m21_ * rhs.m11_ + m22_ * rhs.m21_,
+                m20_ * rhs.m02_ + m21_ * rhs.m12_ + m22_ * rhs.m22_,
+                m20_ * rhs.m03_ + m21_ * rhs.m13_ + m22_ * rhs.m23_ + m23_
+            );
+        }
+        /// Multiply a 4x4 matrix.
+        Matrix4 operator *(const Matrix4& rhs) const
+        {
+            return Matrix4(
+                m00_ * rhs.m00_ + m01_ * rhs.m10_ + m02_ * rhs.m20_ + m03_ * rhs.m30_,
+                m00_ * rhs.m01_ + m01_ * rhs.m11_ + m02_ * rhs.m21_ + m03_ * rhs.m31_,
+                m00_ * rhs.m02_ + m01_ * rhs.m12_ + m02_ * rhs.m22_ + m03_ * rhs.m32_,
+                m00_ * rhs.m03_ + m01_ * rhs.m13_ + m02_ * rhs.m23_ + m03_ * rhs.m33_,
+                m10_ * rhs.m00_ + m11_ * rhs.m10_ + m12_ * rhs.m20_ + m13_ * rhs.m30_,
+                m10_ * rhs.m01_ + m11_ * rhs.m11_ + m12_ * rhs.m21_ + m13_ * rhs.m31_,
+                m10_ * rhs.m02_ + m11_ * rhs.m12_ + m12_ * rhs.m22_ + m13_ * rhs.m32_,
+                m10_ * rhs.m03_ + m11_ * rhs.m13_ + m12_ * rhs.m23_ + m13_ * rhs.m33_,
+                m20_ * rhs.m00_ + m21_ * rhs.m10_ + m22_ * rhs.m20_ + m23_ * rhs.m30_,
+                m20_ * rhs.m01_ + m21_ * rhs.m11_ + m22_ * rhs.m21_ + m23_ * rhs.m31_,
+                m20_ * rhs.m02_ + m21_ * rhs.m12_ + m22_ * rhs.m22_ + m23_ * rhs.m32_,
+                m20_ * rhs.m03_ + m21_ * rhs.m13_ + m22_ * rhs.m23_ + m23_ * rhs.m33_,
+                rhs.m30_,
+                rhs.m31_,
+                rhs.m32_,
+                rhs.m33_
+            );
+        }
+        /// Set translation elements.
+        void SetTranslation(const Vector3& translation)
+        {
+            m03_ = translation.x_;
+            m13_ = translation.y_;
+            m23_ = translation.z_;
+        }
+        /// Set rotation elements from a 3x3 matrix.
+        void SetRotation(const Matrix3& rotation)
+        {
+            m00_ = rotation.m00_;
+            m01_ = rotation.m01_;
+            m02_ = rotation.m02_;
+            m10_ = rotation.m10_;
+            m11_ = rotation.m11_;
+            m12_ = rotation.m12_;
+            m20_ = rotation.m20_;
+            m21_ = rotation.m21_;
+            m22_ = rotation.m22_;
+        }
+
+        /// Set scaling elements.
+        void SetScale(const Vector3& scale)
+        {
+            m00_ = scale.x_;
+            m11_ = scale.y_;
+            m22_ = scale.z_;
+        }
+
+        /// Set uniform scaling elements.
+        void SetScale(float scale)
+        {
+            m00_ = scale;
+            m11_ = scale;
+            m22_ = scale;
+        }
+
+        /// Return the combined rotation and scaling matrix.
+        Matrix3 ToMatrix3() const
+        {
+            return Matrix3(
+                m00_, m01_, m02_,
+                m10_, m11_, m12_,
+                m20_, m21_, m22_
+            );
+        }
+        /// Convert to a 4x4 matrix by filling in an identity last row.
+        Matrix4 ToMatrix4() const
+        {
+            return Matrix4(
+                m00_, m01_, m02_, m03_,
+                m10_, m11_, m12_, m13_,
+                m20_, m21_, m22_, m23_,
+                0.0f, 0.0f, 0.0f, 1.0f
+            );
+        }
+        /// Return the rotation matrix with scaling removed.
+        Matrix3 RotationMatrix() const
+        {
+            Vector3 invScale(
+                1.0f / sqrtf(m00_ * m00_ + m10_ * m10_ + m20_ * m20_),
+                1.0f / sqrtf(m01_ * m01_ + m11_ * m11_ + m21_ * m21_),
+                1.0f / sqrtf(m02_ * m02_ + m12_ * m12_ + m22_ * m22_)
+            );
+
+            return ToMatrix3().Scaled(invScale);
+        }
+        /// Return the translation part.
+        Vector3 Translation() const
+        {
+            return Vector3(m03_, m13_, m23_);
+        }
+        /// Return the scaling part.
+        Vector3 Scale() const
+        {
+            return Vector3(
+                    sqrtf(m00_ * m00_ + m10_ * m10_ + m20_ * m20_),
+                    sqrtf(m01_ * m01_ + m11_ * m11_ + m21_ * m21_),
+                    sqrtf(m02_ * m02_ + m12_ * m12_ + m22_ * m22_)
+            );
+        }
+
+        /// Return the scaling part with the sign. Reference rotation matrix is required to avoid ambiguity.
+        Vector3 SignedScale(const Matrix3& rotation) const
+        {
+            return Vector3(
+                    rotation.m00_ * m00_ + rotation.m10_ * m10_ + rotation.m20_ * m20_,
+                    rotation.m01_ * m01_ + rotation.m11_ * m11_ + rotation.m21_ * m21_,
+                    rotation.m02_ * m02_ + rotation.m12_ * m12_ + rotation.m22_ * m22_
+            );
+        }
+
+        /// Test for equality with another matrix with epsilon.
+        bool Equals(const Matrix3x4& rhs) const
+        {
+            const float* leftData = Data();
+            const float* rightData = rhs.Data();
+
+            for (unsigned i = 0; i < 12; ++i)
+            {
+                if (!My3D::Equals(leftData[i], rightData[i]))
+                    return false;
+            }
+
+            return true;
+        }
         /// Return float data.
         const float* Data() const { return &m00_; }
         /// Return as string.
