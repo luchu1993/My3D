f1fb09b4fd9efdb2fd8f3568ad8a4dcfa3cda471
chu.lu <luchu1993@163.com>
2022-02-08 21:28:29 +0800

    ray

--- Source/Engine/Math/Ray.cpp
+++ Source/Engine/Math/Ray.cpp
@@ -3,8 +3,160 @@
 //
 
 #include "Math/Ray.h"
+#include "Math/Plane.h"
+#include "Math/BoundingBox.h"
+#include "Math/Sphere.h"
+#include "Math/Frustum.h"
+
 
 namespace My3D
 {
+    float Ray::HitDistance(const Plane& plane) const
+    {
+        float d = plane.normal_.DotProduct(direction_);
+        if (Abs(d) >= M_EPSILON)
+        {
+            float t = -(plane.normal_.DotProduct(origin_) + plane.d_) / d;
+            if (t >= 0.0f)
+                return t;
+            else
+                return M_INFINITY;
+        }
+        else
+            return M_INFINITY;
+    }
 
+    float Ray::HitDistance(const BoundingBox& box) const
+    {
+        // If undefined, no hit (infinite distance)
+        if (!box.Defined())
+            return M_INFINITY;
+
+        // Check for ray origin being inside the box
+        if (box.IsInside(origin_))
+            return 0.0f;
+
+        float dist = M_INFINITY;
+
+        // Check for intersecting in the X-direction
+        if (origin_.x_ < box.min_.x_ && direction_.x_ > 0.0f)
+        {
+            float x = (box.min_.x_ - origin_.x_) / direction_.x_;
+            if (x < dist)
+            {
+                Vector3 point = origin_ + x * direction_;
+                if (point.y_ >= box.min_.y_ && point.y_ <= box.max_.y_ && point.z_ >= box.min_.z_ && point.z_ <= box.max_.z_)
+                    dist = x;
+            }
+        }
+        if (origin_.x_ > box.max_.x_ && direction_.x_ < 0.0f)
+        {
+            float x = (box.max_.x_ - origin_.x_) / direction_.x_;
+            if (x < dist)
+            {
+                Vector3 point = origin_ + x * direction_;
+                if (point.y_ >= box.min_.y_ && point.y_ <= box.max_.y_ && point.z_ >= box.min_.z_ && point.z_ <= box.max_.z_)
+                    dist = x;
+            }
+        }
+        // Check for intersecting in the Y-direction
+        if (origin_.y_ < box.min_.y_ && direction_.y_ > 0.0f)
+        {
+            float x = (box.min_.y_ - origin_.y_) / direction_.y_;
+            if (x < dist)
+            {
+                Vector3 point = origin_ + x * direction_;
+                if (point.x_ >= box.min_.x_ && point.x_ <= box.max_.x_ && point.z_ >= box.min_.z_ && point.z_ <= box.max_.z_)
+                    dist = x;
+            }
+        }
+        if (origin_.y_ > box.max_.y_ && direction_.y_ < 0.0f)
+        {
+            float x = (box.max_.y_ - origin_.y_) / direction_.y_;
+            if (x < dist)
+            {
+                Vector3 point = origin_ + x * direction_;
+                if (point.x_ >= box.min_.x_ && point.x_ <= box.max_.x_ && point.z_ >= box.min_.z_ && point.z_ <= box.max_.z_)
+                    dist = x;
+            }
+        }
+        // Check for intersecting in the Z-direction
+        if (origin_.z_ < box.min_.z_ && direction_.z_ > 0.0f)
+        {
+            float x = (box.min_.z_ - origin_.z_) / direction_.z_;
+            if (x < dist)
+            {
+                Vector3 point = origin_ + x * direction_;
+                if (point.x_ >= box.min_.x_ && point.x_ <= box.max_.x_ && point.y_ >= box.min_.y_ && point.y_ <= box.max_.y_)
+                    dist = x;
+            }
+        }
+        if (origin_.z_ > box.max_.z_ && direction_.z_ < 0.0f)
+        {
+            float x = (box.max_.z_ - origin_.z_) / direction_.z_;
+            if (x < dist)
+            {
+                Vector3 point = origin_ + x * direction_;
+                if (point.x_ >= box.min_.x_ && point.x_ <= box.max_.x_ && point.y_ >= box.min_.y_ && point.y_ <= box.max_.y_)
+                    dist = x;
+            }
+        }
+
+        return dist;
+    }
+
+    float Ray::HitDistance(const Frustum& frustum, bool solidInside) const
+    {
+        float maxOutside = 0.0f;
+        float minInside = M_INFINITY;
+        bool allInside = true;
+
+        for (const auto& plane : frustum.planes_)
+        {
+            float distance = HitDistance(plane);
+
+            if (plane.Distance(origin_) < 0.0f)
+            {
+                maxOutside = Max(maxOutside, distance);
+                allInside = false;
+            }
+            else
+                minInside = Min(minInside, distance);
+        }
+
+        if (allInside)
+            return solidInside ? 0.0f : minInside;
+        else if (maxOutside <= minInside)
+            return maxOutside;
+        else
+            return M_INFINITY;
+    }
+
+    float Ray::HitDistance(const Sphere& sphere) const
+    {
+        Vector3 centeredOrigin = origin_ - sphere.center_;
+        float squaredRadius = sphere.radius_ * sphere.radius_;
+
+        // Check if ray originates inside the sphere
+        if (centeredOrigin.LengthSquared() <= squaredRadius)
+            return 0.0f;
+
+        // Calculate intersection by quadratic equation
+        float a = direction_.DotProduct(direction_);
+        float b = 2.0f * centeredOrigin.DotProduct(direction_);
+        float c = centeredOrigin.DotProduct(centeredOrigin) - squaredRadius;
+        float d = b * b - 4.0f * a * c;
+
+        // No solution
+        if (d < 0.0f)
+            return M_INFINITY;
+
+        // Get the nearer solution
+        float dSqrt = sqrtf(d);
+        float dist = (-b - dSqrt) / (2.0f * a);
+        if (dist >= 0.0f)
+            return dist;
+        else
+            return (-b + dSqrt) / (2.0f * a);
+    }
 }
--- Source/Engine/Math/Ray.h
+++ Source/Engine/Math/Ray.h
@@ -8,25 +8,55 @@
 
 namespace My3D
 {
+    class BoundingBox;
+    class Frustum;
+    class Plane;
+    class Sphere;
+
     /// Infinite straight line in three-dimensional space.
     class MY3D_API Ray
     {
         /// Construct a degenerate ray with zero origin and direction.
         Ray() noexcept = default;
-
         /// Construct from origin and direction. The direction will be normalized.
         Ray(const Vector3& origin, const Vector3& direction) noexcept
         {
             Define(origin, direction);
         }
-
+        /// Copy-construct from another ray.
+        Ray(const Ray& ray) noexcept = default;
+        /// Assign from another ray.
+        Ray& operator =(const Ray& rhs) noexcept = default;
+        /// Check for equality with another ray.
+        bool operator ==(const Ray& rhs) const { return origin_ == rhs.origin_ && direction_ == rhs.direction_; }
+        /// Check for inequality with another ray.
+        bool operator !=(const Ray& rhs) const { return origin_ != rhs.origin_ || direction_ != rhs.direction_; }
         /// Define from origin and direction. The direction will be normalized.
         void Define(const Vector3& origin, const Vector3& direction)
         {
             origin_ = origin;
             direction_ = direction.Normalized();
         }
-
+        /// Project a point on the ray.
+        Vector3 Project(const Vector3& point) const
+        {
+            Vector3 offset = point - origin_;
+            return origin_ + offset.DotProduct(direction_) * direction_;
+        }
+        /// Return distance of a point from the ray.
+        float Distance(const Vector3& point) const
+        {
+            Vector3 projected = Project(point);
+            return (point - projected).Length();
+        }
+        /// Return hit distance to a plane, or infinity if no hit.
+        float HitDistance(const Plane& plane) const;
+        /// Return hit distance to a bounding box, or infinity if no hit.
+        float HitDistance(const BoundingBox& box) const;
+        /// Return hit distance to a frustum, or infinity if no hit. If solidInside parameter is true (default) rays originating from inside return zero distance, otherwise the distance to the closest plane.
+        float HitDistance(const Frustum& frustum, bool solidInside = true) const;
+        /// Return hit distance to a sphere, or infinity if no hit.
+        float HitDistance(const Sphere& sphere) const;
         /// Ray origin.
         Vector3 origin_;
         /// Ray direction.
