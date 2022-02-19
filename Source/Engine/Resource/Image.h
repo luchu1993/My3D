66aed4027d020247b9e56c772301d89309478312
chu.lu <luchu1993@163.com>
2022-02-19 10:01:13 +0800

    color and image

--- Source/Engine/Resource/Image.h
+++ Source/Engine/Resource/Image.h
@@ -11,6 +11,8 @@ struct SDL_Surface;
 
 namespace My3D
 {
+    static const int COLOR_LUT_SIZE = 16;
+
     /// Supported compressed image formats.
     enum CompressedFormat
     {
@@ -73,7 +75,60 @@ namespace My3D
         bool Save(Serializer& dest) const override;
         /// Save the image to a file. Format of the image is determined by file extension. JPG is saved with maximum quality.
         bool SaveFile(const String& fileName) const override;
-
+        /// Set 2D size and number of color components. Old image data will be destroyed and new data is undefined. Return true if successful.
+        bool SetSize(int width, int height, unsigned components);
+        /// Set 3D size and number of color components. Old image data will be destroyed and new data is undefined. Return true if successful.
+        bool SetSize(int width, int height, int depth, unsigned components);
+        /// Set new image data.
+        void SetData(const unsigned char* pixelData);
+        /// Set a 2D pixel.
+        void SetPixel(int x, int y, const Color& color);
+        /// Set a 3D pixel.
+        void SetPixel(int x, int y, int z, const Color& color);
+        /// Set a 2D pixel with an integer color. R component is in the 8 lowest bits.
+        void SetPixelInt(int x, int y, unsigned uintColor);
+        /// Set a 3D pixel with an integer color. R component is in the 8 lowest bits.
+        void SetPixelInt(int x, int y, int z, unsigned uintColor);
+        /// Load as color LUT. Return true if successful.
+        bool LoadColorLUT(Deserializer& source);
+        /// Flip image horizontally. Return true if successful.
+        bool FlipHorizontal();
+        /// Flip image vertically. Return true if successful.
+        bool FlipVertical();
+        /// Resize image by bilinear resampling. Return true if successful.
+        bool Resize(int width, int height);
+        /// Clear the image with a color.
+        void Clear(const Color& color);
+        /// Clear the image with an integer color. R component is in the 8 lowest bits.
+        void ClearInt(unsigned uintColor);
+        /// Save in BMP format. Return true if successful.
+        bool SaveBMP(const String& fileName) const;
+        /// Save in PNG format. Return true if successful.
+        bool SavePNG(const String& fileName) const;
+        /// Save in TGA format. Return true if successful.
+        bool SaveTGA(const String& fileName) const;
+        /// Save in JPG format with specified quality. Return true if successful.
+        bool SaveJPG(const String& fileName, int quality) const;
+        /// Save in DDS format. Only uncompressed RGBA images are supported. Return true if successful.
+        bool SaveDDS(const String& fileName) const;
+        /// Whether this texture is detected as a cubemap, only relevant for DDS.
+        bool IsCubemap() const { return cubemap_; }
+        /// Whether this texture has been detected as a volume, only relevant for DDS.
+        bool IsArray() const { return array_; }
+        /// Whether this texture is in sRGB, only relevant for DDS.
+        bool IsSRGB() const { return sRGB_; }
+        /// Return a 2D pixel color.
+        Color GetPixel(int x, int y) const;
+        /// Return a 3D pixel color.
+        Color GetPixel(int x, int y, int z) const;
+        /// Return a 2D pixel integer color. R component is in the 8 lowest bits.
+        unsigned GetPixelInt(int x, int y) const;
+        /// Return a 3D pixel integer color. R component is in the 8 lowest bits.
+        unsigned GetPixelInt(int x, int y, int z) const;
+        /// Return a bilinearly sampled 2D pixel color. X and Y have the range 0-1.
+        Color GetPixelBilinear(float x, float y) const;
+        /// Return a trilinearly sampled 3D pixel color. X, Y and Z have the range 0-1.
+        Color GetPixelTrilinear(float x, float y, float z) const;
         /// Return width.
         int GetWidth() const { return width_; }
         /// Return height.
@@ -90,6 +145,33 @@ namespace My3D
         CompressedFormat GetCompressedFormat() const { return compressedFormat_; }
         /// Return number of compressed mip levels. Returns 0 if the image is has not been loaded from a source file containing multiple mip levels.
         unsigned GetNumCompressedLevels() const { return numCompressedLevels_; }
+        /// Return next mip level by bilinear filtering. Note that if the image is already 1x1x1, will keep returning an image of that size.
+        SharedPtr<Image> GetNextLevel() const;
+        /// Return the next sibling image of an array or cubemap.
+        SharedPtr<Image> GetNextSibling() const { return nextSibling_;  }
+        /// Return image converted to 4-component (RGBA) to circumvent modern rendering API's not supporting e.g. the luminance-alpha format.
+        SharedPtr<Image> ConvertToRGBA() const;
+        /// Return a compressed mip level.
+        CompressedLevel GetCompressedLevel(unsigned index) const;
+        /// Return decompressed image data in RGBA format.
+        SharedPtr<Image> GetDecompressedImage() const;
+        /// Return subimage from the image by the defined rect or null if failed. 3D images are not supported. You must free the subimage yourself.
+        Image* GetSubimage(const IntRect& rect) const;
+        /// Return an SDL surface from the image, or null if failed. Only RGB images are supported. Specify rect to only return partial image. You must free the surface yourself.
+        SDL_Surface* GetSDLSurface(const IntRect& rect = IntRect::ZERO) const;
+        /// Precalculate the mip levels. Used by asynchronous texture loading.
+        void PrecalculateLevels();
+        /// Whether this texture has an alpha channel.
+        /// @property
+        bool HasAlphaChannel() const;
+        /// Copy contents of the image into the defined rect, scaling if necessary. This image should already be large enough to include the rect. Compressed and 3D images are not supported.
+        bool SetSubimage(const Image* image, const IntRect& rect);
+        /// Clean up the mip levels.
+        void CleanupLevels();
+        /// Get all stored mip levels starting from this.
+        void GetLevels(PODVector<Image*>& levels);
+        /// Get all stored mip levels starting from this.
+        void GetLevels(PODVector<const Image*>& levels) const;
 
     private:
         /// Decode an image using stb_image.
